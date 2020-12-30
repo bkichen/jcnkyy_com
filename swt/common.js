5f7454e60ef25f54d859acad315df8aabbff7514
root <root@localhost.localdomain>
2020-12-31 00:01:42 +0800

    up

---
+++ swt/common.js
@@ -0,0 +1,86 @@
+//8秒关闭
+document.writeln('<script type="text/javascript" src="http://yuyue.0514800.com/swt/8miao.js" charset="UTF-8"></script>');
+
+
+// JavaScript Document
+//document.writeln("<style>");
+//document.writeln("#LRfloater1,#LRdiv0{ display:none !important;}");
+//document.writeln("</style>");
+document.writeln("<div style=\"display:none;\">");
+/*百度统计*/
+document.writeln("<script>");
+document.writeln("var _hmt = _hmt || [];");
+document.writeln("(function() {");
+document.writeln("  var hm = document.createElement(\"script\");");
+document.writeln("  hm.src = \"//hm.baidu.com/hm.js?72339e0335a9712e80d239e0c726fe25\";");
+document.writeln("  var s = document.getElementsByTagName(\"script\")[0]; ");
+document.writeln("  s.parentNode.insertBefore(hm, s);");
+document.writeln("})();");
+document.writeln("</script>");
+document.writeln("</div>");
+document.writeln("<!--商务通-->");
+
+document.writeln("<div style=\"display:none;\">");
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ddff2423d2d7c1540103cc15d1c44e29";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+document.writeln("</div>");
+
+
+if(typeof jQuery == 'undefined'){
+	document.writeln('<script type="text/javascript" src="http://t.0514nk.com/public/js/jquery-1.8.0.min.js"></script>');
+}
+
+document.writeln("<script language=\"javascript\" src=\"http://yuyue.0514800.com/swt/renai_kst_pc.js\"><\/script>");
+//中间弹窗
+document.write('<script type="text/javascript" src="http://cdn.0514800.com/yzraseo/yd_swtcenter.js" charset="gbk"></script>')
+
+document.writeln("<style>");
+document.writeln("ul,li{margin:0;padding:0}");
+document.writeln("li{list-style-type:none}");
+document.writeln(".rightss{ display:none}");
+document.writeln("@media only screen and (max-width:1023px) {.rightss{ display:block}}");
+document.writeln(".swt_bottom_1218 li a{ text-decoration:none}");
+document.writeln(".zhgh_gh{margin-bottom: 3rem !important;}");
+document.writeln(".swt_bottom_1218 { width: 100%; overflow: hidden;position: fixed;bottom: 0;left:0;z-index:8888;}");
+document.writeln(".swt_bottom_1218 li { float: left; width: 25%; text-align: center; font-size: 15px; background: #f4f4f4; padding-bottom: 8px;}");
+document.writeln(".swt_bottom_1218 li i { display: block; margin: 15px auto 7.5px; }");
+document.writeln(".swt_bottom_1218 li:first-child i { width: 42px; height: 42px; background: url(/swt/swt_bottom_1218_04.png) no-repeat; background-size: 100%;margin-top: 8px;}");
+document.writeln(".swt_bottom_1218 li:nth-child(2) i { width: 41.25px; height: 41.25px; background: url(/swt/swt_bottom_1218_06.gif) no-repeat; background-size: 100%; margin-top: 8.55px; }");
+document.writeln(".swt_bottom_1218 li b { display: block; width: 91.5px; height: 91.5px; background: #f4f4f4; border-radius: 50%; margin: 0 auto; margin-top: -34.5px; overflow: hidden; }");
+document.writeln(".swt_bottom_1218 li a {color:#333;}");
+document.writeln(".swt_bottom_1218 li:nth-child(3) i {width: 36px; height: 36px; background: url(/swt/swt_bottom_tel.png) no-repeat; background-size: 100%; margin-top: 14px; }");
+document.writeln(".swt_bottom_1218 li:nth-child(3) span { display: block; width: 67.5px; height: 67.5px; border-radius: 50%; position: absolute; left:50%; margin-left:-33.75px; top: -22.5px; z-index: 1; -webkit-box-shadow: 0 0 30px #8f3900; box-shadow: 0 0 30px #8f3900; -webkit-animation: sonarEffect 1.5s ease-out 1s infinite; animation: 1.5s ease-out 1s normal none infinite running sonarEffect; }");
+document.writeln(".swt_bottom_1218 li:nth-child(4) i { width: 33.75px; height: 31.5px; background: url(/swt/swt_bottom_1218_07.png) no-repeat; background-size: 100%; margin-top:15.5px; margin-bottom: 10.5px; }");
+document.writeln("@keyframes sonarEffect {");
+document.writeln("0% {opacity: 0.3;filter:alpha(opacity:30);}");
+document.writeln("40% {-webkit-shadow: 0 0 0 1px rgba(143,57,108,0.1), 0 0 2px 2px #fff, 0 0 0 4px rgba(255,184,108,0.5);box-shadow: 0 0 0 1px rgba(143,57,108,0.1), 0 0 2px 2px #fff, 0 0 0 4px rgba(255,184,108,0.5);opacity: 0.5;filter:alpha(opacity:50);}");
+document.writeln("100% {-webkit-shadow: 0 0 0 1px rgba(143,57,108,0.1), 0 0 2px 2px #fff, 0 0 0 4px rgba(255,184,108,0.5);box-shadow: 0 0 0 1px rgba(143,57,108,0.1), 0 0 2px 2px #fff, 0 0 0 4px rgba(255,184,108,0.5);opacity: 0;filter:alpha(opacity:0);-webkit-transform: scale(1.5);transform: scale(1.5);}");
+document.writeln("}");
+document.writeln("@-webkit-keyframes sonarEffect {");
+document.writeln("0% {opacity: 0.3;filter:alpha(opacity:30);}");
+document.writeln("40% {-webkit-shadow: 0 0 0 1px rgba(143,57,108,0.1), 0 0 2px 2px #fff, 0 0 0 4px rgba(255,184,108,0.5);box-shadow: 0 0 0 1px rgba(143,57,108,0.1), 0 0 2px 2px #fff, 0 0 0 4px rgba(255,184,108,0.5);opacity: 0.5;filter:alpha(opacity:50);}");
+document.writeln("100% {-webkit-shadow: 0 0 0 1px rgba(143,57,108,0.1), 0 0 2px 2px #fff, 0 0 0 4px rgba(255,184,108,0.5);box-shadow: 0 0 0 1px rgba(143,57,108,0.1), 0 0 2px 2px #fff, 0 0 0 4px rgba(255,184,108,0.5);opacity: 0;filter:alpha(opacity:0);-webkit-transform: scale(1.5);transform: scale(1.5);}");
+document.writeln("}");
+document.writeln("	</style>");
+document.writeln("<ul class=\"swt_bottom_1218 rightss\">");
+document.writeln("	<li> <a href=\"/\"> <i></i> 网站首页 </a> </li>");
+document.writeln("	<li> <a href=\"/kefu.html?dibu\"> <i></i> 在线咨询 </a> </li>");
+document.writeln("	<li> <a href=\"tel:19952836735\"> <i></i> 一键通话 </a> </li>");
+document.writeln("	<li> <a href=\"/kefu.html?dibu\"> <i></i> 预约挂号 </a> </li>");
+document.writeln("</ul>");
+document.writeln("");
+$(function(){
+	dibuheight=$('.swt_bottom_1218').outerHeight();
+	heights=window.screen.width;
+	if (heights<1023)
+	{
+	$("body").css("padding-bottom",dibuheight);
+	}
+})
